19feee5157ac819a81ab16f1b3f1da21a71b8b6f
ZhaoZhipeng <i@mezzp.com>
2022-10-16 23:09:08 +0800

    :hammer: add vaultwarden

---
+++ Linux/vaultwarden/vaultwarden.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 控制台颜色
+GREEN="\033[1;32m"
+RESET="$(tput sgr0)"
+
+docker-compose -f docker-compose-vaultwarden.yml -p vaultwarden up -d
+
+printf "${GREEN}"
+cat <<EOF
+=======vaultwarden已启动=======
+      访问地址：ip:3001
+===============================
+EOF
+printf "${RESET}"
